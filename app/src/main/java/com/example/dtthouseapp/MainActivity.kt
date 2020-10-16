516107a15ba42caeb22238ae9d210ebcfd31ea10
Huncho Jack <=>
2020-10-16 23:29:32 +0200

    just tested the get request and oh baby it works. added the header for the access key with the api key. works beautifully on the first try. I displayed the response on a toast message as a quick test before I move on. things are going beautiful

--- app/src/main/java/com/example/dtthouseapp/MainActivity.kt
+++ app/src/main/java/com/example/dtthouseapp/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.dtthouseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repo = HouseRepo(HouseApi())
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val houses = repo.getHouses()
+            Toast.makeText(this@MainActivity, houses.toString(), Toast.LENGTH_LONG).show()
+            Log.i("main", houses.toString())
+        }
+
 
 
     }
