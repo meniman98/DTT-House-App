3102a37c1c9723cd48d20b90c085ebd332a283cf
Huncho Jack <=>
2020-10-31 19:44:53 +0100

    added the coroutines file

--- app/src/main/java/com/example/dtthouseapp/HousesFragment.kt
+++ app/src/main/java/com/example/dtthouseapp/HousesFragment.kt
@@ -6,11 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 
 class HousesFragment : Fragment() {
 
-
+    private lateinit var factory: HousesViewModelFactory
     private lateinit var viewModel: HousesViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +24,10 @@ class HousesFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val api = HouseApi()
+        val repo = HouseRepo(api)
+        factory = HousesViewModelFactory(repo)
+        viewModel = ViewModelProviders.of(this).get(HousesViewModel::class.java)
 
 
     }
