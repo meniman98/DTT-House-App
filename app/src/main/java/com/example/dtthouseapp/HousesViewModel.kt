ae3fcb2cc32cbed93b109a13460f1c9bf46b7f2a
Huncho Jack <=>
2020-10-31 20:48:29 +0100

    added a job object in the viewmodel

--- app/src/main/java/com/example/dtthouseapp/HousesViewModel.kt
+++ app/src/main/java/com/example/dtthouseapp/HousesViewModel.kt
@@ -3,10 +3,13 @@ package com.example.dtthouseapp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
 
 class HousesViewModel(
     private val repo: HouseRepo
 ) : ViewModel() {
+
+    private lateinit var job: Job
 
     private val private_houses = MutableLiveData<List<HouseItem>>()
     val houses : LiveData<List<HouseItem>>
@@ -14,8 +17,14 @@ class HousesViewModel(
         // not exposed
     get() = private_houses
 
-    suspend fun getHouses() {
-        val houses = repo.getHouses()
-        private_houses.value = houses
+     fun getHouses() {
+         job = Coroutines.ioMain(
+             {repo.getHouses()},
+             { private_houses.value = it }
+
+         )
+
+
+
     }
 }
